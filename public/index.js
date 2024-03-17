91ae0825efa8a8e4b17dc629b7064ce6eebabae1
Will Hutcheon <willhutcheon@gmail.com>
2024-03-17 17:12:47 -0400

    hopefully the .gitignore works

---
+++ public/index.js
@@ -0,0 +1,25 @@
+let categories = ['funnyJoke', 'lameJoke'];
+let funnyJoke = [
+    {
+        'joke': 'Why did the student eat his homework?',
+        'response': 'Because the teacher told him it was a piece of cake!'
+    },
+    {
+        'joke': 'What kind of tree fits in your hand?',
+        'response': 'A palm tree'
+    },
+    {
+        'joke': 'What is worse than raining cats and dogs?',
+        'response': 'Hailing taxis'
+    }
+];
+let lameJoke = [
+    {
+        'joke': 'Which bear is the most condescending?',
+        'response': 'Pan-DUH'
+    },
+    {
+        'joke': 'What would the Terminator be called in his retirement?',
+        'response': 'The Exterminator'
+    }
+];
